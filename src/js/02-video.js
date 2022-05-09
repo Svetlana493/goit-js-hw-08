9bde4d361153efc601792796a3bdef8b11976361
Svetlana Fedorenko <svetlana493@ukr.net>
2022-05-09 21:04:36 +0300

    дз 8 зро

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,16 @@
-// Ознакомься с документацией библиотеки Vimeo плеера.
-// Добавь библиотеку как зависимость проекта через npm.
-// Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time"
+
+const onPlay = function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+const savedCurrentTime = localStorage.getItem(STORAGE_KEY) ? localStorage.getItem(STORAGE_KEY) : 0;
+
+player.setCurrentTime(savedCurrentTime);
+
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+var throttle = require('lodash.throttle');
 
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+}
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener("submit", onFormSubmit);
+
+
+populateTextArea()
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+
+function populateTextArea() {
+    const savedMasseg = localStorage.getItem(STORAGE_KEY);
+    const parsedsavedMasseg = JSON.parse(savedMasseg)
+
+    if (savedMasseg) {
+        refs.form.elements.message.value = parsedsavedMasseg.message || "";
+        refs.form.elements.email.value = parsedsavedMasseg.email || "";
+
+    }
+}
